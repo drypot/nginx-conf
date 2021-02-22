c43563366d9036f8b4523065bc232d30f0183a8d
drypot <drypot@gmail.com>
2021-02-22 17:48:12 +0000

    update.

--- aws1/d-run.sh
+++ aws1/d-run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 args=(
   --name nginx
-  -p 80:80
-  -p 443:443
+  --network=host
+  #-p 80:80
+  #-p 443:443
   --mount type=bind,source=/data/nginx/nginx-conf/aws1/nginx.conf,target=/etc/nginx/nginx.conf,readonly
   --mount type=bind,source=/data/nginx/nginx-conf/aws1/enabled,target=/etc/nginx/enabled,readonly
   --mount type=bind,source=/data/nginx/tmp,target=/data/nginx/tmp
   --mount type=bind,source=/var/lib/letsencrypt,target=/var/lib/letsencrypt,readonly
   --mount type=bind,source=/data/certbot/letsencrypt,target=/etc/letsencrypt,readonly
   --mount type=bind,source=/data/service,target=/data/service,readonly
+  --mount type=bind,source=/data/upload,target=/data/upload,readonly
   -it --rm
 #  -d
   nginx
